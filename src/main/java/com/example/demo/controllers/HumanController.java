ee1dd050f94b583e07438773fa02efd24da6145a
Vepr <isip_a.n.veprickiy@mpt.ru>
2022-11-23 14:32:28 +0300

    пр3

--- src/main/java/com/example/demo/controllers/HumanController.java
+++ src/main/java/com/example/demo/controllers/HumanController.java
@@ -1,16 +1,20 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Human;
+import com.example.demo.models.Post;
 import com.example.demo.repo.HumanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HumanController {
@@ -47,5 +51,61 @@ public class HumanController {
         //List<Human> result = humanRepository.findByWeightContains(weight);
         model.addAttribute("result", result);
         return "human-main";
+    }
+
+
+    @GetMapping("/human/{id}/edit")
+    public  String HumanDetails(@PathVariable(value = "id") long id, Model model)
+    {
+        Optional<Human> post = humanRepository.findById(id);
+        ArrayList<Human> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("human",res);
+        if(!humanRepository.existsById(id))
+        {
+            return  "redirect:/human";
+        }
+        return "human-main";
+    }
+
+
+    @PostMapping ("/human/{id}/edit")
+    public  String HumanPostUpdate(@PathVariable(value = "id") long id,
+                                  @RequestParam(defaultValue = "")  String lastName,
+                                  @RequestParam(defaultValue = "0") float height,
+                                  @RequestParam(defaultValue = "false")  boolean gender,
+                                  @RequestParam(defaultValue = "10.10.2010") Date birthday,
+                                  @RequestParam(defaultValue = "0")  double weight,
+                                  Model model)
+    {
+        Human human = humanRepository.findById(id).orElseThrow();
+        human.setLastName(lastName);
+        human.setHeight(height);
+        human.setGender(gender);
+        human.setBirthday(birthday);
+        human.setWeight(weight);
+        humanRepository.save(human);
+        return "redirect:/human";
+    }
+
+
+    @GetMapping("/human/{id}/remove")
+    public  String HumanDelDetails(@PathVariable(value = "id") long id, Model model)
+    {
+        Optional<Human> post = humanRepository.findById(id);
+        ArrayList<Human> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("human",res);
+        if(!humanRepository.existsById(id))
+        {
+            return  "redirect:/human";
+        }
+        return HumanDelete(id,model);
+    }
+    @PostMapping("/human/{id}/remove")
+    public String HumanDelete(@PathVariable(value = "id") long id, Model model){
+        Human human = humanRepository.findById(id).orElseThrow();
+        humanRepository.delete(human);
+        return "redirect:/human";
     }
 }
